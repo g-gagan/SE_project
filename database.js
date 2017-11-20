630556f37e948490c4ee9ced4d7d035205f038e2
g-gagan <gagan.28.1.97@gmail.com>
2017-11-20 09:59:50 +0530

    Changing

--- database.js
+++ database.js
@@ -1,15 +1,16 @@
 var Connection = require('tedious').Connection;
 var Request = require('tedious').Request;
+var TYPES = require('tedious').TYPES;
 
 // Create connection to database
 var config = 
    {
-     userName: 'admin123', // update me
-     password: 'SEproject123!', // update me
-     server: 'paperevalserver.database.windows.net', // update me
+     userName: 'admin123', 
+     password: 'SEproject123!',
+     server: 'paperevalserver.database.windows.net',
      options: 
         {
-           database: 'paperEvalDB' //update me
+           database: 'paperEvalDB' 
            , encrypt: true
         }
    }
@@ -24,67 +25,45 @@ connection.on('connect', function(err)
        }
     else
        {
-           queryDatabase()
+          console.log("Connected to DB");
+          queryDatabase()
        }
    }
  );
+
+
+
 
 function queryDatabase()
 { 
     console.log('Reading rows from the Table...');
 
    // Read all rows from table
-    request = new Request(
-        /*"SELECT TOP 20 pc.Name as CategoryName, p.name as ProductName FROM [SalesLT].[ProductCategory] pc JOIN [SalesLT].[Product] p ON pc.productcategoryid = p.productcategoryid",
+/*    request = new Request(
+        // "SELECT TOP 20 pc.Name as CategoryName, p.name as ProductName FROM [SalesLT].[ProductCategory] pc JOIN [SalesLT].[Product] p ON pc.productcategoryid = p.productcategoryid",
+        "SELECT * FROM login WHERE UserName=@name AND Password=@pwd",
              function(err, rowCount, rows) 
             {
                 console.log(rowCount + ' row(s) returned');
                 process.exit();
-            }*/
+            }
+        );*/
 
-        SELECT version();
-        );
+  // var sql = 'SELECT * FROM login WHERE UserName=@name AND Password=@pwd';
+  var sql = 'SELECT * FROM login WHERE UserName=@name AND Password=@pwd';
+  var request = new Request(sql, function(err) {
+    if(err) console.log(err);
+  });
+
+  request.addParameter('name', TYPES.VarChar, 'jyotsna.prasad3@gmail.com');
+  request.addParameter('pwd', TYPES.VarChar, 'jyo123');
+
+  connection.execSql(request);
 
     request.on('row', function(columns) {
         columns.forEach(function(column) {
         console.log("%s\t%s", column.metadata.colName, column.value);
      });
          });
- connection.execSql(request);
+ //connection.execSql(request);
 }
-
-
-
-
-/*
-const mysql = require('mysql');
-
-var exports = {};
-var connection = null;
-
-exports.createConnection = function() {
-    connection = mysql.createConnection({
-        host: 'paperevalserver.database.windows.net',
-        user: 'admin123',
-        password: 'admin123',
-        database: 'paperEvalDB',
-        port: 1337,
-    	ssl: true
-    });
-    connection.connect((err) => {
-        if (err) throw err;
-        console.log('Connected to DB');
-    });
-};
-
-module.exports = exports;
-
-//checks the user and password from the database 
-exports.checkLoginEntry = function(UserName, password, completeWithStatus) {
-    connection.query('SELECT * FROM `Login` WHERE `User Name`=? AND `Password`=?', [UserName.toUpperCase(), password], (err, results, fields) => {
-        if (err) throw err;
-        if (results.length == 0) return completeWithStatus(404);
-        else return completeWithStatus(200);
-    });
-};
-*/
